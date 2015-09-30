9658bc73001f506889ce92eefe811502fc24b82d
Luca Mele <lucamele1@hotmail.com>
2015-09-30 11:57:45 +0200

    basic server ready

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+
+http.createServer(app).listen(3000);
