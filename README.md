67322b94dabeaecc49b0452e1cfd75644a6c5cee
Luca Mele <lucamele1@hotmail.com>
2015-10-03 16:35:07 +0200

    Typescript setup

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Group 9 CAS_FEE final project with angular.js
 First install all npm dependencies:
 
 `npm install`
+`npm install -g gulp`
+`npm install -g bower`
+
+Link global gulp locally
+
+`npm link gulp`
 
 and then start the application with:
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,40 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var typescript = require('gulp-typescript');
+var watch = require('gulp-watch');
+var typescriptAngular = require('gulp-typescript-angular');
+var concat = require('gulp-concat');
+var p = require('./package.json');
 
 gulp.task('bower', function() {
 	return bower()
 		.pipe(gulp.dest('public/lib/'))
 });
 
-gulp.task('default', ['bower']);
+
+gulp.task('compile', function () {
+	return gulp.src(
+		[
+			'./app/**/*.ts'
+		])
+		.pipe(typescript({
+			target:'es5'
+		}))
+		.pipe(typescriptAngular({
+			decoratorModuleName:'app'
+		}))
+		.pipe(gulp.dest('./public/tmp'));
+});
+
+gulp.task('unify', ['compile'], function() {
+	return gulp.src('./public/tmp/**/*.js')
+		.pipe(concat(p.name + '_' + p.version + '.js'))
+		.pipe(gulp.dest('./public/dist'));
+});
+
+gulp.task('default', ['bower', 'compile', 'unify'], function() {
+	watch('./app/**/*.ts', function () {
+		gulp.start('compile');
+		gulp.start('unify');
+	});
+});
