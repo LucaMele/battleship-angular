1c84906db3ecb2b6f94f1d71e39de19275063b22
Luca Mele <lucamele1@hotmail.com>
2015-11-07 15:05:31 +0100

    first login done with answer.

--- server/modules/login/index.js
+++ server/modules/login/index.js
@@ -1,22 +1,47 @@
+var crypto = require('crypto');
+
 /**
  * Created by Luca on 25.10.2015.
  */
 function LoginModule(db, assert){
-
     var users;
 
-    db.open(function(err, db) {
-        assert.equal(null, err);
-        users = db.createCollection('users');
-        // Close the connection with a callback that is optional
-        db.close(function(err, result) {
-            assert.equal(null, err);
+    var md5 = function(string) {
+        var md5sum = crypto.createHash('md5');
+        return  md5sum.update(string).digest("hex");
+    };
 
+    this.post = function(req, res) {
+        var user = req.body;
+        user.password = md5(user.password);
+        var cursor = db.collection('users').find(user).limit(1);
+        cursor.count(function(err, count) {
+            if (count === 0) {
+                res.status(401).send({ error: 'unauthorized!' });
+            } else {
+                cursor.forEach(function(doc){
+                    var auth = md5(doc._id.toString()) + ''+ md5(Date.now() + 'gdjnxhzw');
+                    var date = new Date();
+                    date.setDate(date.getDate() + 1);
+                    db.collection('users').updateOne({ _id: doc._id },
+                        { $set:
+                            {
+                                auth: {
+                                    key: auth,
+                                    expires: date.getTime()
+                                }
+                            }
+                        }, function(err, doc) {
+                            res.format({
+                                'application/json': function(){
+                                    res.send({ auth: auth});
+                                }
+                            });
+                        }
+                    );
+                });
+            }
         });
-    });
-
-    this.post = function() {
-        console.log('db', users);
     }
 }
 
--- server/services/db.js
+++ server/services/db.js
@@ -1,8 +1,13 @@
 var Db = require('mongodb').Db,
     MongoClient = require('mongodb').MongoClient,
     Server = require('mongodb').Server,
-    config = require('../config');
+    config = require('../config'),
+    assert = require('assert');
 
 var db = new Db(config.db.name, new Server(config.db.host, config.db.port));
+
+db.open(function(err, db) {
+    assert.equal(null, err);
+});
 // Establish connection to db
 module.exports = {db: db, MongoClient: MongoClient};
