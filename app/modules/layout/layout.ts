aeddd58a62eeec28e7ed8aed106debdc5d4277e9
Ricardo Marcelo Voegeli <rvoegeli@gmail.com>
2015-11-08 13:14:47 +0100

    layout

--- app/modules/layout/layout.ts
+++ app/modules/layout/layout.ts
@@ -5,22 +5,62 @@
 module app.layout{
     export var identifier:string = 'layout';
 
+
+    @app.Controller
+    class LayoutController implements appComponent{
+
+        static $inject = [
+            '$scope'
+        ];
+        static $componentName = 'layoutController';
+        public componentName;
+
+        constructor($scope) {
+            this.componentName = 'layoutController';
+            console.log($scope, this);
+
+        }
+    }
+
     @app.Directive
     export class LayoutDirective implements appComponent{
 
         static $inject = [
+            '$templateCache'
         ];
         static $componentName = 'layout';
         public componentName;
+        public $templateCache;
 
-        constructor() {
+        //public templateUrl = '/myApp/templates/eeeeeee.htm';
+        public replace = true;
+        public restrict = 'E';
+        public template = function(jqlite, attributes){
+            console.log(attributes.ricardo)
+            return this.$templateCache.get(LayoutDirective.$componentName + '/templates/header.html');
+        };
+
+        public controller;
+
+        constructor($templateCache) {
             this.componentName = LayoutDirective.$componentName;
+            this.$templateCache = $templateCache;
+            this.controller = LayoutController;
             return this;
+            /*return {
+                restrict: 'A',
+                replace: true,
+                require:'ngModel',
+                scope: {
+                    ssn: '=ngModel',
+                    disabled:'=ngDisabled'
+                },
+                //templateUrl points to an external html template.
+                templateUrl: '/myApp/templates/ssnControl.htm'
+            }*/
         }
 
-        public template = 'Ricardo';
 
-        public restrict = 'E';
 
         /**
          *
