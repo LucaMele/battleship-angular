b20d1c8df4fa454808186041ec8486199de4f3a9
Ricardo Marcelo Voegeli <rvoegeli@gmail.com>
2015-11-16 19:50:44 +0100

    config changes for local

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ var config = {
     availableModules: ['login', 'home', 'usersList', 'user', 'game'],
     db: {
         name: 'gZ3-m__battleship',
-        host: 'lucamele.cloudapp.net',
+        host: 'localhost',//'lucamele.cloudapp.net',
         port: 27017
     }
 };
