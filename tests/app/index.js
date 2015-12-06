2c4df8bbc9772c7a5540cd45d8284b807bacc32d
Luca Mele <lucamele1@hotmail.com>
2015-12-06 10:22:41 +0100

    temp test

--- tests/app/index.js
+++ tests/app/index.js
@@ -1,7 +1,16 @@
 /**
  * Created by Luca on 05.12.2015.
  */
-describe('sorting the list of users', function() {
+describe('Register the login controller', function() {
+    beforeEach(module('app'));
+
+    var $controller;
+
+    beforeEach(inject(function(_$controller_){
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+    }));
+
     it('sorts in descending order by default', function() {
         var users = ['jack', 'igor', 'jeff'];
         var sorted = ['jeff', 'jack', 'igor'];
