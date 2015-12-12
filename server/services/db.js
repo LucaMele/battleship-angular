bfa0764b37ea754420f673c801caa1902d363e28
Luca Mele <lucamele1@hotmail.com>
2015-12-12 17:13:08 +0100

    reactivate db

--- server/services/db.js
+++ server/services/db.js
@@ -8,10 +8,9 @@ var db = new Db(config.db.name, new Server(config.db.host, config.db.port));
 
 db.open(function(err, db) {
     assert.equal(null, err);
-/*
     db.authenticate(config.db.user, config.db.password, function(err, res) {
         // callback
-    });*/
+    });
 });
 // Establish connection to db
 module.exports = {db: db, MongoClient: MongoClient};
